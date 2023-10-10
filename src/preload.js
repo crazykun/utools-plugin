27818a776e6fdf86530e3142ecf07678c339a34a
jilikun <jilikun@gmail.com>
2023-10-10 16:30:22 +0800

    复制base64结果

--- src/preload.js
+++ src/preload.js
@@ -15,8 +15,6 @@ window.encodeFn = function () {
     AES: AES, //AES cbc加密
     AES2: AES2, //AES ecb加密
     DES: DES, //DES加密
-    TripleDES: TripleDES, //TripleDES加密
-    Rabbit: Rabbit, //Rabbit加密
   };
   return fn;
 };
@@ -84,36 +82,23 @@ function AES(str, key, iv) {
   encrypted += cipher.final("hex");
   return encrypted;
 }
-function AES2(str, key, iv) {
-  iv = fixlength(iv, 16);
+
+function AES2(str, key) {
   key = fixlength(key, 32);
-  const cipher = crypto.createCipheriv("aes-256-ecb", key, iv);
+  const cipher = crypto.createCipheriv("aes-256-ecb", key, null);
   let encrypted = cipher.update(str, "utf8", "hex");
   encrypted += cipher.final("hex");
   return encrypted;
 }
 
-function DES(str, key, iv) {
-  // key 256bit iv 128bit
-  const cipher = crypto.createCipheriv("des-cbc", key, iv);
+function DES(str, key) {
+  key = fixlength(key, 8);
+  const cipher = crypto.createCipheriv("des-ecb", key, null);
   let encrypted = cipher.update(str, "utf8", "hex");
   encrypted += cipher.final("hex");
   return encrypted;
 }
 
-function TripleDES(str, key, iv) {
-  const cipher = crypto.createCipheriv("des-ede3-cbc", key, iv);
-  let encrypted = cipher.update(str, "utf8", "hex");
-  encrypted += cipher.final("hex");
-  return encrypted;
-}
-
-function Rabbit(str, key, iv) {
-  const cipher = crypto.createCipheriv("rabbit", key, iv);
-  let encrypted = cipher.update(str, "utf8", "hex");
-  encrypted += cipher.final("hex");
-  return encrypted;
-}
 
 // 解密
 window.decodeFn = function () {
@@ -121,8 +106,6 @@ window.decodeFn = function () {
     AES: _AES, //AES解密
     AES2: _AES2, //AES解密
     DES: _DES, //DES解密
-    TripleDES: _TripleDES, //TripleDES解密
-    Rabbit: _Rabbit, //Rabbit解密
   };
   return fn;
 };
@@ -135,35 +118,22 @@ function _AES(str, key, iv) {
   decrypted += decipher.final("utf8");
   return decrypted;
 }
-function _AES2(str, key, iv) {
-  iv = fixlength(iv, 16);
+function _AES2(str, key) {
   key = fixlength(key, 32);
-  const decipher = crypto.createDecipheriv("aes-256-ecb", key, iv);
+  const decipher = crypto.createDecipheriv("aes-256-ecb", key, null);
   let decrypted = decipher.update(str, "hex", "utf8");
   decrypted += decipher.final("utf8");
   return decrypted;
 }
 
-function _DES(str, key, iv) {
-  const decipher = crypto.createDecipheriv("des-cbc", key, iv);
+function _DES(str, key) {
+  key = fixlength(key, 8);
+  const decipher = crypto.createDecipheriv("des-ecb", key, null);
   let decrypted = decipher.update(str, "hex", "utf8");
   decrypted += decipher.final("utf8");
   return decrypted;
 }
 
-function _TripleDES(str, key, iv) {
-  const decipher = crypto.createDecipheriv("des-ede3-cbc", key, iv);
-  let decrypted = decipher.update(str, "hex", "utf8");
-  decrypted += decipher.final("utf8");
-  return decrypted;
-}
-
-function _Rabbit(str, key, iv) {
-  const decipher = crypto.createDecipheriv("rabbit", key, iv);
-  let decrypted = decipher.update(str, "hex", "utf8");
-  decrypted += decipher.final("utf8");
-  return decrypted;
-}
 
 
 function fixlength(str, lenght) {
